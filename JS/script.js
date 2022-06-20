b263cf323a21c43332be24dc01bb88d022a08584
Jessemccallums <jessemccallums2001@gmail.com>
2022-06-20 05:30:30 -0400

    Add mobile menu and changes

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,27 @@
 
+  const toggleMenu = document.querySelector('#toggle-menu');
+  const closeBtn = document.querySelector('#close-btn');
+  const menuItems = document.querySelectorAll('.menu-item');
+  // for-top-nav
+  function openNav() {
+    document.querySelector('.mobile-menu').classList.add('active');
+    toggleMenu.classList.add('invisible');
+    document.body.style.overflowY = 'hidden';
+  }
+  
+  toggleMenu.addEventListener('click', openNav);
+  
+  function closeNav() {
+    document.querySelector('.mobile-menu').classList.remove('active');
+    toggleMenu.classList.remove('invisible');
+    document.body.style.overflowY = 'auto';
+  }
+  
+  closeBtn.addEventListener('click', closeNav);
+  
+  menuItems.forEach((item) => {
+    item.addEventListener('click', closeNav);
+  });
 //  Speakers
 const speakers = [
     {
@@ -56,3 +79,5 @@ const speakers = [
       moreSpeakerButton.style.display = 'none';
     },
   );
+
+
